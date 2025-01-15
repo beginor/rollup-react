0ef9c79722430f8055bd043dba82d73505c7ed59
beginor <beginor@qq.com>
2025-01-15 15:06:46 +0800

    remove esbuild

--- packages/shared/esbuild.js
+++
@@ -1,8 +0,0 @@
-import { createOptions, buildOrWatch } from '../../scripts/esbuild-helper.js';
-
-const options = createOptions(
-  ['./src/index.ts'],
-  './dist/index.js'
-);
-
-buildOrWatch(options);
